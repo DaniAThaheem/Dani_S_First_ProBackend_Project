6d6248a12b92d65f9b0be288cf0aa7f08333a515
DaniAThaheem <danimsbzuacc@outlook.com>
2025-04-18 09:47:23 +0500

    updated the app.js file

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ import subscriptionRouter from "./routes/subscription.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 const app = express()
 
@@ -44,3 +48,7 @@ app.use("/api/v1/video", videoRouter)
 app.use("/api/v1/subscription", subscriptionRouter)
 app.use("/api/v1/tweet", tweetRouter)
 app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/like", likeRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/dashboardRouter", dashboardRouter)
+app.use("/api/v1/comment", commentRouter)
